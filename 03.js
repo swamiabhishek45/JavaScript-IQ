325cf108d70f94039f330b83cbb65c4445b4f6c8
swamiabhishek45 <abhishekswami1435@gmail.com>
2024-03-06 22:29:37 +0530

    word question

--- 03.js
+++ 03.js
@@ -3,13 +3,25 @@
 // var arr = [1,2,3,4,5]; // Input, multiply each element by 2
 // var newArr = [2,4,6,8,10] // Output with new array
 
-var arr = [1,2,3,4,5];
-var arr2 = [];
+// var arr = [1,2,3,4,5];
+// var arr2 = [];
 
-arr.forEach((val) => {
-    arr2.push(val * 2);
+// arr.forEach((val) => {
+//     arr2.push(val * 2);
+// })
+
+// console.log(arr2);
+
+var words = ["Abhishek", "Omkar", "Nitin","Vishal","Prasad"];
+
+const ans = words.map((word) => {
+    if(word.length === 5) {
+        return `<p>${word}</p>`
+    } else {
+        return `<span>${word}</span>`
+    }
 })
 
-console.log(arr2);
+console.log(ans);
 
 
