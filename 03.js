1cc0e23622bc22e0bba7b2f52fa3e8dab0d6f173
swamiabhishek45 <abhishekswami1435@gmail.com>
2024-03-06 22:08:36 +0530

    map

---
+++ 03.js
@@ -0,0 +1,15 @@
+// map -> if you want to perform operation on each element on an array and store it in new array
+// return is compulsory in map
+// var arr = [1,2,3,4,5]; // Input, multiply each element by 2
+// var newArr = [2,4,6,8,10] // Output with new array
+
+var arr = [1,2,3,4,5];
+var arr2 = [];
+
+arr.forEach((val) => {
+    arr2.push(val * 2);
+})
+
+console.log(arr2);
+
+
