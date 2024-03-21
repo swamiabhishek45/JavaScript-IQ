2be00f4087eac140780883d5efc3edcb8de26e7b
swamiabhishek45 <abhishekswami1435@gmail.com>
2024-03-21 15:46:50 +0530

    "Added object properties and values in different ways: constructor, create method, and prototype; updated function constructor to include prototype"
    This commit adds new properties and values to an existing object using different methods: object constructor, Object's create method, and function constructor with prototype. Additionally, it updates the function constructor to include a prototype property.

--- 02Folder/01_object-ways.js
+++ 02Folder/01_object-ways.js
@@ -6,3 +6,67 @@ var obj = {
     name: "Abhi",
     age: 21
 }
+obj.height = 5.8
+// console.log(obj);
+
+// 2. Object constructor
+
+var obj = new Object();
+obj.name = "Abhi";
+obj.age = 21;
+obj.height = 5.8;
+// console.log(obj);
+
+// 3. Object's create method
+
+let vehicle = {
+  wheels: "4",
+  fuelType: "Gasoline",
+  color: "Green",
+};
+let carProps = {
+  type: {
+    value: "Volkswagen",
+  },
+  model: {
+    value: "Golf",
+  },
+};
+
+var car = Object.create(vehicle, carProps);
+car.name = "Mercedes"
+
+// console.log(car.color);
+// console.log(car.type);
+// console.log(vehicle.color);
+
+// 4. Function constructor
+function Person(name){
+  this.name = name;
+  this.age = 21;
+}
+
+var obj = new Person("Abhishek");
+
+// console.log(obj);
+
+// 5. Function constructor with prototype
+function Person(){}
+Person.prototype.name = "Abhishek";
+var obj = new Person();
+
+// console.log(obj);
+// console.log(prototype);
+
+function func1() {
+  setTimeout(() => {
+    console.log(x);
+    console.log(y);
+  }, 3000);
+
+  var x = 2;
+  let y = 12;
+}
+func1();
+  
+  
