efa2954c614726f3ffc9541da6132e31689dfe26
swamiabhishek45 <abhishekswami1435@gmail.com>
2024-03-22 18:47:44 +0530

    "Updated rectangle color based on mouse position using GSAP"

--- Js Practice/Q1 Rectangle/script.js
+++ Js Practice/Q1 Rectangle/script.js
@@ -7,58 +7,58 @@ rectangle.addEventListener("mousemove", function (details) {
 
   var insideRectVal = mouseLoc - rectLoc.left;
 
-  //   if (insideRectVal < rectLoc.width / 2) {
-  //     var redColor = gsap.utils.mapRange(
-  //       0,
-  //       rectLoc.width / 2,
-  //       255,
-  //       0,
-  //       insideRectVal
-  //     );
-  //     gsap.to(rectangle, {
-  //       backgroundColor: `rgb(${redColor}, 0, 0)`,
-  //       ease: Power4,
-  //     });
-  //   } else {
-  //     var greenColor = gsap.utils.mapRange(
-  //       rectLoc.width / 2,
-  //       rectLoc.width,
-  //       0,
-  //       255,
-  //       insideRectVal
-  //     );
-  //     gsap.to(rectangle, {
-  //       backgroundColor: `rgb(0, ${greenColor}, 0)`,
-  //       ease: Power4,
-  //     });
-  //   }
+    if (insideRectVal < rectLoc.width / 2) {
+      var redColor = gsap.utils.mapRange(
+        0,
+        rectLoc.width / 2,
+        255,
+        0,
+        insideRectVal
+      );
+      gsap.to(rectangle, {
+        backgroundColor: `rgb(${redColor}, 0, 0)`,
+        ease: Power4,
+      });
+    } else {
+      var greenColor = gsap.utils.mapRange(
+        rectLoc.width / 2,
+        rectLoc.width,
+        0,
+        255,
+        insideRectVal
+      );
+      gsap.to(rectangle, {
+        backgroundColor: `rgb(0, ${greenColor}, 0)`,
+        ease: Power4,
+      });
+    }
 
-  if (insideRectVal < rectLoc.width / 2) {
-    var redColor = gsap.utils.mapRange(
-      0,
-      rectLoc.width / 2,
-      255,
-      0,
-      insideRectVal
-    );
+  // if (insideRectVal < rectLoc.width / 2) {
+  //   var redColor = gsap.utils.mapRange(
+  //     0,
+  //     rectLoc.width / 2,
+  //     255,
+  //     0,
+  //     insideRectVal
+  //   );
 
-    rectangle.style.backgroundColor = `rgb(${redColor}, 0, 0)`;
-  } else {
-    var greenColor = gsap.utils.mapRange(
-      rectLoc.width / 2,
-      rectLoc.width,
-      0,
-      255,
-      insideRectVal
-    );
+  //   rectangle.style.backgroundColor = `rgb(${redColor}, 0, 0)`;
+  // } else {
+  //   var greenColor = gsap.utils.mapRange(
+  //     rectLoc.width / 2,
+  //     rectLoc.width,
+  //     0,
+  //     255,
+  //     insideRectVal
+  //   );
 
-    rectangle.style.backgroundColor = `rgb(0, ${greenColor}, 0)`;
-  }
+  //   rectangle.style.backgroundColor = `rgb(0, ${greenColor}, 0)`;
+  // }
 });
 
 rectangle.addEventListener("mouseleave", function () {
-  // gsap.to(rectangle, {
-  //     backgroundColor: "white"
-  // })
-  rectangle.style.backgroundColor = "white";
+  gsap.to(rectangle, {
+      backgroundColor: "white"
+  })
+  // rectangle.style.backgroundColor = "white";
 });
