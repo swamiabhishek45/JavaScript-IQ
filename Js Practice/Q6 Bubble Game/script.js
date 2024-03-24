70960df57c786cd49ca16855254850ec207078ab
swamiabhishek45 <abhishekswami1435@gmail.com>
2024-03-24 15:05:40 +0530

    create runTimer function

--- Js Practice/Q6 Bubble Game/script.js
+++ Js Practice/Q6 Bubble Game/script.js
@@ -1,10 +1,28 @@
-const pbtm = document.querySelector(".pbtm");
+var time = 60;
+function createBubble() {
+  const pbtm = document.querySelector(".pbtm");
 
-var clutter = "";
+  var clutter = "";
 
-for(var i = 1; i<=114; i++){
-    clutter += `<div class="bubble">10</div>`;
+  for (var i = 1; i <= 114; i++) {
+    var num = Math.floor(Math.random() * 100);
+    clutter += `<div class="bubble">${num}</div>`;
+  }
+
+  pbtm.innerHTML = clutter;
 }
 
-pbtm.innerHTML = clutter;
+function runTimer() {
+  const timer = document.querySelector("#timer");
+  var ID = setInterval(function () {
+    if (time > 0) {
+      time--;
+      timer.textContent = time;
+    } else {
+      clearInterval(ID);
+    }
+  }, 1000);
+}
 
+runTimer();
+createBubble();
