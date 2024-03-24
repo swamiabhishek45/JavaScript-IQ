b9af4ec1dd4a7c16fcacdeb72e40ae1396205445
swamiabhishek45 <abhishekswami1435@gmail.com>
2024-03-24 17:33:35 +0530

    "Updated Bubble Game with timer, score, and game over functionality"

--- Js Practice/Q6 Bubble Game/script.js
+++ Js Practice/Q6 Bubble Game/script.js
@@ -1,28 +1,68 @@
-var time = 60;
+const pbtm = document.querySelector(".pbtm");
+const timer = document.querySelector("#timer");
+const hit = document.querySelector("#hit");
+const score = document.querySelector("#score");
+
+var time = 5;
+var scoreVal = 0;
+
 function createBubble() {
-  const pbtm = document.querySelector(".pbtm");
-
   var clutter = "";
-
   for (var i = 1; i <= 114; i++) {
-    var num = Math.floor(Math.random() * 100);
+    var num = Math.floor(Math.random() * 10);
     clutter += `<div class="bubble">${num}</div>`;
   }
-
   pbtm.innerHTML = clutter;
 }
 
+function getNewHit() {
+  var num = Math.floor(Math.random() * 10);
+  hit.textContent = num;
+}
+
 function runTimer() {
-  const timer = document.querySelector("#timer");
   var ID = setInterval(function () {
     if (time > 0) {
       time--;
       timer.textContent = time;
     } else {
       clearInterval(ID);
+      pbtm.innerHTML = `<div class="gameOver">
+      <h1>GAME OVER</h1>
+      <button onclick = location.reload() >Restart Game</button>
+      </div>`;
     }
   }, 1000);
 }
 
-runTimer();
+function increaseScore() {
+  scoreVal += 10;
+  score.textContent = scoreVal;
+}
+
+function decreaseScore() {
+  if (scoreVal > 0) {
+    scoreVal -= 10;
+    score.textContent = scoreVal;
+  }
+}
+
+pbtm.addEventListener("click", function (dets) {
+  const clickedNum = Number(dets.target.textContent);
+  const targetElement = dets.target;
+  if (clickedNum === parseInt(hit.textContent)) {
+    increaseScore();
+    getNewHit();
+    createBubble();
+  } else if (clickedNum !== parseInt(hit.textContent)) {
+    decreaseScore();
+    targetElement.style.backgroundColor = "red";
+    setTimeout(function () {
+      targetElement.style.backgroundColor = "rgb(77, 128, 77)";
+    }, 1000);
+  }
+});
+
+getNewHit();
 createBubble();
+runTimer();
