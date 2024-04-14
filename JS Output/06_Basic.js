a8d8a5d8b57e1fb6446dcdca7f53717f2342c628
swamiabhishek45 <abhishekswami1435@gmail.com>
2024-04-14 22:23:07 +0530

    change

---
+++ JS Output/06_Basic.js
@@ -0,0 +1,19 @@
+// let x = 10;
+// function foo() {
+//     console.log(x); // undefined
+//     var x = 20;
+// }
+// foo();
+
+// console.log('5' + 3); // "53"
+// console.log('5' - 3); // 2
+// console.log('5' * 3); // 15
+// console.log('9' / 3); // 3
+
+// console.log(5 == "5"); // true --> check type only
+// console.log(5 === "5"); // false --> check both type and datatype
+
+// console.log(typeof NaN); // number --> +"hi" -> here we try to covert string into number but it's not possible bcz it gives NaN but its datatype is Number so typeof Nan returns number
+
+console.log(1 < 2 < 3); // true
+console.log(3 > 2 > 1); // false
