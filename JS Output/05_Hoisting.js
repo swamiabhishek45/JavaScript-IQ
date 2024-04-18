5d93f2e50ad2044bf7c3a86143230212f9944edb
swamiabhishek45 <abhishekswami1435@gmail.com>
2024-04-18 22:36:39 +0530

    promise

--- JS Output/05_Hoisting.js
+++ JS Output/05_Hoisting.js
@@ -122,3 +122,17 @@
 // delete obj.height;
 // console.log(obj.height);
 
+
+
+
+function a() {
+    console.log("A");
+}
+
+setTimeout(() => console.log("B"), 0);
+
+a();
+
+console.log("C");
+
+Promise.resolve().then(() => console.log("D"));
