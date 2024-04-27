13061797bf67dbf8c677af0e0f6d6697eeb685e5
swamiabhishek45 <abhishekswami1435@gmail.com>
2024-04-27 21:52:56 +0530

    nea category filed

--- JS Mini Projects/Q12 Meme Generator2.0/script.js
+++ JS Mini Projects/Q12 Meme Generator2.0/script.js
@@ -1,36 +1,71 @@
 const draggable = document.querySelectorAll(".draggable");
 const dropZone = document.querySelectorAll(".drop-zone");
-const imgUpload = document.getElementById('imgUpload');
+const imgUpload = document.getElementById("imgUpload");
+
+// custom category
+
+document.querySelector("#addCategory").addEventListener("click", function () {
+    const categoryName = document.getElementById("categoryName").value.trim();
+    const categoryColor = document.getElementById("categoryColor").value;
+
+    if (categoryName.length === "") {
+        alert("Please enter a category name");
+        return;
+    }
+
+    const categoryContainer = document.getElementById("categories");
+    const newCategory = document.createElement("div");
+
+    newCategory.classList.add(
+        "category",
+        "drop-zone",
+        "flex",
+        "flex-row",
+        "space-x-3",
+        "overflow-auto",
+        "m-8"
+    );
+
+    newCategory.style.backgroundColor = categoryColor;
+
+    newCategory.innerHTML = `<h2 class="text-xl p-3 font-semibold">${categoryName}</h2>`;
+
+    categoryContainer.appendChild(newCategory);
+
+    // category drag and drop
+
+
+});
 
 // image upload
 
-imgUpload.addEventListener('change', function (e) {
-    const imgContainer = document.getElementById('imgContainer');
+imgUpload.addEventListener("change", function (e) {
+    const imgContainer = document.getElementById("imgContainer");
     const files = e.target.files;
 
-    for(let i = 0; i < files.length; i++){
+    for (let i = 0; i < files.length; i++) {
         const file = files[i];
         const reader = new FileReader();
-        reader.onload = function(e) {
-            const img = document.createElement('img');
+        reader.onload = function (e) {
+            const img = document.createElement("img");
             img.src = e.target.result;
-            img.classList.add("draggable", "img-size");                                                                                                                                                                     
+            img.classList.add("draggable", "img-size");
             img.alt = file.name;
             img.id = "uploadedImage" + i;
             imgContainer.appendChild(img);
 
             // image drag and drop
-            img.addEventListener('dragstart', e => {
-                e.dataTransfer.setData('text', e.target.id);
+            img.addEventListener("dragstart", (e) => {
+                e.dataTransfer.setData("text", e.target.id);
                 console.log("test", e.target.id);
-            })
-        }
-
+            });
+        };
 
         reader.readAsDataURL(file);
     }
-})
+});
 
+// functional code
 draggable.forEach((item) => {
     item.addEventListener("dragstart", (e) => {
         e.dataTransfer.setData("text", e.target.id);
