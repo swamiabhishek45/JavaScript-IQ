a9b136783c403cc5992ceb7b0027ecfb577e65fc
swamiabhishek45 <abhishekswami1435@gmail.com>
2024-07-20 18:45:49 +0530

    add comment container

--- JS Mini Projects/Q13 Comment Box/script.js
+++ JS Mini Projects/Q13 Comment Box/script.js
@@ -1,6 +1,40 @@
-var field = document.querySelector('textarea');
+var field = document.querySelector("textarea");
+var bakUp = field.getAttribute("placeholder");
+var btn = document.querySelector("#btn");
+var clear = document.querySelector("#clear");
+const commentsContainer = document.querySelector("#commentContainer");
+const commentBtn = document.querySelector("#commentBtn");
 
-field.onfocus = function(){
-    this.setAttribute('placeholder', '');
-    this.style.borderColor = '#333';
-}
+field.onfocus = function () {
+    this.setAttribute("placeholder", "");
+    this.style.borderColor = "#333";
+    btn.style.display = "block";
+};
+
+field.onblur = function () {
+    this.setAttribute("placeholder", bakUp);
+    this.style.borderColor = "#888";
+};
+
+clear.onclick = function () {
+    btn.style.display = "none";
+    field.value = "";
+};
+
+commentBtn.onclick = function () {
+    // Get the value of the comment input
+    const commnetText = field.value;
+
+    // Check if the commnet is not empty
+
+    if (commnetText.trim() !== "") {
+        // create a new div for comment
+        const commentElem = document.createElement("div");
+        // commentElem.className = "";
+        commentElem.textContent = commnetText;
+
+        // Add the comment to comment container
+        commentsContainer.appendChild(commentElem);
+        field.value = "";
+    }
+};
