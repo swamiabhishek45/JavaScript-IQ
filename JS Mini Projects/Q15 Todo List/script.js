80e60a8c3f7349280a5abcf0931acd84a835983c
swamiabhishek45 <abhishekswami1435@gmail.com>
2024-07-28 21:25:24 +0530

    todo list app done

---
+++ JS Mini Projects/Q15 Todo List/script.js
@@ -0,0 +1,42 @@
+const inputBox = document.querySelector("#input-box");
+const listContainer = document.querySelector("#list-container");
+
+function addTask() {
+    if (inputBox.value === "") {
+        alert("Please enter a task");
+    } else {
+        const li = document.createElement("li");
+        li.textContent = inputBox.value;
+        listContainer.appendChild(li);
+
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7"; // x
+        li.appendChild(span);
+    }
+    inputBox.value = "";
+    saveData();
+}
+
+listContainer.addEventListener(
+    "click",
+    function (e) {
+        if (e.target.tagName === "LI") {
+            e.target.classList.toggle("checked");
+            saveData();
+        } else if (e.target.tagName === "SPAN") {
+            e.target.parentElement.remove();
+            saveData();
+        }
+    },
+    false
+);
+
+function saveData() {
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function showList() {
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+
+showList();
