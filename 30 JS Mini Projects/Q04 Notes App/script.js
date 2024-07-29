06dfd9a2858f9b67b27f92ab371ffdaf67429aa7
swamiabhishek45 <abhishekswami1435@gmail.com>
2024-07-29 20:38:28 +0530

    notes app done

--- 30 JS Mini Projects/Q04 Notes App/script.js
+++ 30 JS Mini Projects/Q04 Notes App/script.js
@@ -1,29 +1,34 @@
 const container = document.querySelector(".notes-container");
+let notes = document.querySelectorAll(".input-box");
+
+function showNotes() {
+    container.innerHTML = localStorage.getItem("notes");
+}
+function saveNotes() {
+    localStorage.setItem("notes", container.innerHTML);
+}
 
 function addNote() {
-    const p = document.createElement("p");
-    p.setAttribute("contenteditable", true);
-
-    const img = document.createElement("img");
+    let inputBox = document.createElement("p");
+    let img = document.createElement("img");
+    inputBox.className = "input-box";
+    inputBox.setAttribute("contenteditable", true);
     img.src = "delete.png";
-
-    container.appendChild(p).appendChild(img);
-    saveNotes();
+    container.appendChild(inputBox).appendChild(img);
 }
 
 container.addEventListener("click", function (e) {
     if (e.target.tagName === "IMG") {
         e.target.parentElement.remove();
         saveNotes();
+    } else if (e.target.tagName === "P") {
+        notes = document.querySelectorAll(".input-box");
+        notes.forEach((note) => {
+            note.onkeyup = function () {
+                saveNotes();
+            };
+        });
     }
 });
-
-function saveNotes(e) {
-    localStorage.setItem("notes", container.innerHTML);
-}
-
-function showNotes() {
-    container.innerHTML = localStorage.getItem("notes");
-}
 
 showNotes();
