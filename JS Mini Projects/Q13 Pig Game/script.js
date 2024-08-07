898ef780d61cc2dc4d6f20d9cd91ddb5793dcaf1
swamiabhishek45 <abhishekswami1435@gmail.com>
2024-08-07 22:03:10 +0530

    issue solved

--- JS Mini Projects/Q13 Pig Game/script.js
+++ JS Mini Projects/Q13 Pig Game/script.js
@@ -1,7 +1,8 @@
 const model = document.querySelector(".model");
 const overlay = document.querySelector(".overlay");
 const btnCloseModel = document.querySelector(".close-model");
-const btnOpenModel = document.querySelector(".show-model");
+const btnOpenModel = document.querySelectorAll(".show-model");
+console.log(btnOpenModel);
 
 const openModel = function () {
     model.classList.remove("hidden");
@@ -13,11 +14,17 @@ const closeModel = function () {
     overlay.classList.add("hidden");
 };
 
-for(let i = 0 ;i<btnOpenModel.length; i++){
+for (let i = 0; i < btnOpenModel.length; i++) {
     btnOpenModel[i].addEventListener("click", openModel);
+    console.log('click');
 }
 
+
 btnCloseModel.addEventListener("click", closeModel);
+overlay.addEventListener("click", closeModel);
 
-overlay.addEventListener('click', closeModel)
-
+document.addEventListener("keydown", function (e) {
+    if (e.key == "Escape" && !model.classList.contains("hidden")) {
+        closeModel();
+    }
+});
